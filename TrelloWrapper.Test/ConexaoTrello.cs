b0168622c3425c087e00103c86d017a37f77f8d5
Hamon Vitorino <hamon.vitorino@gmail.com>
2015-12-04 10:53:16 -0300

    Primeiro commit

---
+++ TrelloWrapper.Test/ConexaoTrello.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System.Diagnostics;
+using TrelloNet;
+
+namespace TrelloWrapper.Test
+{
+    [TestFixture]
+    public class ConexaoTrello
+    {
+        [Test]
+        public void ConetarTrelloNet()
+        {
+            var trello = new Trello("950a4f35f078102cc55be50178a55181");
+            trello.Authorize("7fd6a41ed8d4b253b9f49465f025898b980b103ddea5a4bcbee243230d9e9783");
+
+            foreach (var team in trello.Organizations.Search("S160"))
+            {
+                Debug.WriteLine(team.DisplayName);
+            }
+        }
+    }
+}
