1fded849ffedf99e108aac17c4b2002d6cf50978
Hamon Vitorino <hamon.vitorino@gmail.com>
2015-12-07 17:12:37 -0300

    Setup nao estava executando portanto foi remmovido e a conexao esta no metodo de teste

--- TrelloWrapper.Test/RecuperarTime.cs
+++ TrelloWrapper.Test/RecuperarTime.cs
@@ -1,6 +1,5 @@
 ﻿using NUnit.Framework;
 using System.Linq;
-using System.Diagnostics;
 using TrelloNet;
 
 namespace TrelloWrapper.Test
@@ -11,25 +10,13 @@ namespace TrelloWrapper.Test
         private const string chave = "950a4f35f078102cc55be50178a55181";
         private const string token = "6b7d13c413e8d89cd85e27f1e094f10d879f44c5f18b27f8c594d4cd9b051e9c";
 
-        private Trello trello;
-
-        [OneTimeSetUp]
-        public void ConectarTrello()
-        {
-            trello = new Trello(chave);
-            trello.Authorize(token);
-        }
-
-        [OneTimeTearDown]
-        public void Desconectar()
-        {
-            trello.Deauthorize();
-        }
-
         [Test]
         public void RecuperarS160()
         {
-            var org = trello.Organizations.Search("S160").Single();
+            var trello = new Trello(chave);
+            trello.Authorize(token);
+
+            var org = trello.Organizations.Search("s160").SingleOrDefault();
 
             Assert.That(org, Is.Not.Null);
             Assert.That(org.Name.ToLower(), Is.EqualTo("s160"));
