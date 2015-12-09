17d8bec8aab3b5339c004721ce59bfaabaf127b5
Hamon Vitorino <hamon.vitorino@gmail.com>
2015-12-09 10:48:24 -0300

    Removendo cartoes criados durante os testes

--- TrelloWrapper.Test/CriarCartao/QualquerSeveridade.cs
+++ TrelloWrapper.Test/CriarCartao/QualquerSeveridade.cs
@@ -1,11 +1,16 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Linq;
+using TrelloNet;
 
 namespace TrelloWrapper.Test.CriarCartao
 {
     [TestFixture]
     public class QualquerSeveridade
     {
+        private const string chave = "950a4f35f078102cc55be50178a55181";
+        private const string token = "6b7d13c413e8d89cd85e27f1e094f10d879f44c5f18b27f8c594d4cd9b051e9c";
+
         private Cartao cartao;
         private Incidente incidente;
         private Treller treller;
@@ -24,6 +29,26 @@ namespace TrelloWrapper.Test.CriarCartao
             };
 
             cartao = treller.cadastrarIncidente(incidente);
+        }
+
+        [OneTimeTearDown]
+        public void DesfazCenario()
+        {
+            var trello = new Trello(chave);
+            trello.Authorize(token);
+
+            var quadroS160 = trello.Organizations.WithId("s160");
+
+            var incidentes = trello.Boards.ForOrganization(quadroS160)
+                .Where(board => board.Name.Equals("incidentes", StringComparison.OrdinalIgnoreCase))
+                .FirstOrDefault();
+
+            var allCards = trello.Cards.ForBoard(incidentes);
+
+            foreach (var card in allCards)
+            {
+                trello.Cards.Delete(card);
+            }
         }
 
         [Test]
