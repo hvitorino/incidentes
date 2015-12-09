511b76ba84f3a41b41d9effc753a4a59da1e61a9
Hamon Vitorino <hamon.vitorino@gmail.com>
2015-12-09 14:56:35 -0300

    Movendo cartao rem resolucao

---
+++ TrelloWrapper.Test/MoverCartaoEmResolucao.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace TrelloWrapper.Test
+{
+    [TestFixture]
+    public class MoverCartaoEmResolucao : TesteComCenario
+    {
+        private Cartao cartao;
+        private Incidente incidente;
+        private Treller treller;
+
+        [OneTimeSetUp]
+        public void Cenario()
+        {
+            treller = new Treller();
+
+            incidente = new Incidente
+            {
+                Id = "EM_INVESTIGACAO_MOVIDO",
+                Severidade = NivelSeveridade.Alta,
+                Sistema = "S160",
+                DataSubmissao = DateTime.Now
+            };
+
+            cartao = treller.cadastrarIncidente(incidente);
+        }
+
+        [Test]
+        public void PossoMoverParaPendencia()
+        {
+            treller.moverParaPendencia(cartao);
+
+            Assert.That(cartao.Lista, Is.EqualTo(ListaEstado.Pendencia));
+        }
+    }
+}
